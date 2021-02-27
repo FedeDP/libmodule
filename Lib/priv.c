e1cb688a4086751aab40d8f2271c5b8a67b5ed6f
Federico Di Pierro <nierro92@gmail.com>
2021-02-27 23:23:34 +0100

    Use CLOCK_MONOTONIC on bsd as CLOCK_BOOTTIME is linux-specific.

--- Lib/priv.c
+++ Lib/priv.c
@@ -22,7 +22,11 @@ char *mem_strdup(const char *s) {
 
 void fetch_ms(uint64_t *val, uint64_t *ctr) {
     struct timespec spec;
+#ifdef __linux__
     clock_gettime(CLOCK_BOOTTIME, &spec);
+#else
+    clock_gettime(CLOCK_MONOTONIC, &spec);
+#endif
     *val = spec.tv_sec * 1000 + spec.tv_nsec / 1000000;
     
     if (ctr) {
