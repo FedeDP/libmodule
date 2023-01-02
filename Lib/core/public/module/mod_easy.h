18026082a968cd25c3f21a0c903e2a585d93feb9
Federico Di Pierro <nierro92@gmail.com>
2023-01-02 18:49:45 +0100

    cleanup(core): dropped libmodule_(de)init functions and ctors.
    Signed-off-by: Federico Di Pierro <nierro92@gmail.com>

--- Lib/core/public/module/mod_easy.h
+++ Lib/core/public/module/mod_easy.h
@@ -6,6 +6,20 @@
 
 #define M_CTX_DEFAULT           "libmodule"
 
+/*
+ * ctors order:
+ * 0) global m_on_boot(), useful to set your own memhook if desired
+ * 1) each m_mod_on_boot() (only mod_easy API)
+ * 2) each m_mod_ctor() (only mod_easy API)
+ *
+ * dtors order:
+ * 0) each m_mod_dtor() (only mod_easy API)
+ */
+#define _m_ctor0_         __attribute__((constructor (110)))
+#define _m_ctor1_         __attribute__((constructor (113)))
+#define _m_ctor2_         __attribute__((constructor (114)))
+#define _m_dtor0_         __attribute__((destructor (111)))
+
 /* Simple macro to automatically manage module lifecycle and callbacks */
 #define M_MOD(name) \
     static bool m_mod_on_start(m_mod_t *mod); \
@@ -13,7 +27,7 @@
     static void m_mod_on_evt(m_mod_t *mod, const m_queue_t *const evts); \
     static void m_mod_on_stop(m_mod_t *mod); \
     static m_ctx_t *_m_ctx; \
-    static void _m_ctor3_ m_m_ctor(void) { \
+    static void _m_ctor2_ m_m_ctor(void) { \
         _m_ctx = m_ctx(); \
         if (!_m_ctx) { \
             m_ctx_register(M_CTX_DEFAULT, &_m_ctx, 0, NULL); \
@@ -21,7 +35,7 @@
         m_mod_hook_t hook = { m_mod_on_start, m_mod_on_eval, m_mod_on_evt, m_mod_on_stop }; \
         m_mod_register(name, _m_ctx, NULL , &hook, M_MOD_PERSIST, NULL); \
     } \
-    static void _m_dtor1_ m_mod_dtor(void) { \
+    static void _m_dtor0_ m_mod_dtor(void) { \
         m_mem_unrefp((void **)&_m_ctx); \
     } \
-    static void _m_ctor2_ m_mod_on_boot(void)
+    static void _m_ctor1_ m_mod_on_boot(void)
